b1c84275cd3a18195cdd600d509fd861179f0a1c
Guilherme <=>
2025-01-04 13:26:26 -0300

    primeiras estilizações

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,1 +1,28 @@
 import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 55px;
+    background-color:rgb(32, 32, 101);
+
+    display: flex;
+    align-items:center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+
+    input {
+        width: 100%;
+        height: 25px;
+        background-color: rgb(32, 32, 101);
+        border: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        padding: 0 10px;
+        color: #FFF;
+        float: right;
+        clear: both;
+    }
+`
